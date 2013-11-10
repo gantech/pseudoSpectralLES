1e0653792b134ab3893979411f08f8638f5d5866
Ganesh Vijayakumar <gantech@krakenpf4.nics.utk.edu>
2013-11-10 16:55:09 -0500

    Missed two files in the last commit

--- abl/Sullivan/postprocess/FAST/submitJob.sh
+++ abl/Sullivan/postprocess/FAST/submitJob.sh
@@ -1,6 +1,15 @@
+for (( i=1; i<=15; i++ ))
+do 
+    cp fast.pbs temp.pbs 
+    echo $i 
+    sed "s/blah/$i/g" temp.pbs > temp1.pbs 
+    mv temp1.pbs fast_${i}_${i}.pbs 
+done
 cd /lustre/scratch/gantech/les_abl/Sullivan/data/sullivanTest/case037/FAST/
 cp ~/pseudoSpectralLES/abl/Sullivan/postprocess/FAST/FASTfilesWrite .
-cp ~/pseudoSpectralLES/abl/Sullivan/postprocess/FAST/fast.pbs .
-cp ~/pseudoSpectralLES/abl/Sullivan/postprocess/FAST/run15Rows .
-qsub fast.pbs
+cp ~/pseudoSpectralLES/abl/Sullivan/postprocess/FAST/fast_*.pbs .
+for (( i=2; i<=15; i++ ))
+do 
+    qsub fast_${i}_${i}.pbs
+done
 cd -

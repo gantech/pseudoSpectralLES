5f03cb146fddc53101ae5758632f4eb10d980268
Ganesh Vijayakumar <gantech@krakenpf3.nics.utk.edu>
2013-11-15 20:50:04 -0500

    Fixed the plotting file for spectra. Added a few bells and whistles.

--- abl/Sullivan/postprocess/statisticalAnalysis/spectra/spectra.py
+++ abl/Sullivan/postprocess/statisticalAnalysis/spectra/spectra.py
@@ -1,4 +1,4 @@
-from pylab import loadtxt, transpose, size
+from pylab import loadtxt, transpose, size, logspace, ones
 nLevels = 50
 nx = 768
 xl = 5120.0
@@ -14,19 +14,22 @@ dz = 8.0
 del data
 
 if __name__=="__main__":
-    import matplotlib
     import matplotlib.pyplot as p
-    matplotlib.use('Agg')
-    
+
+    print logspace(-7,3,11)
     #Make spectra plots at each level
     for z in range(size(zLevels)):
         p.figure()
-        p.semilogx(wavenumber,u[z,:],label='U')
-        p.semilogx(wavenumber,v[z,:],label='V')
-        p.semilogx(wavenumber,w[z,:],label='W')
+        p.loglog(wavenumber,u[z,:],label='U')
+        p.loglog(wavenumber,v[z,:],label='V')
+        p.loglog(wavenumber,w[z,:],label='W')
+        km5by3 = 200.0*wavenumber**(-5.0/3.0) 
+        p.loglog(wavenumber, km5by3,'k--',label='km5by3')
+        p.loglog(w[z,:].argmax() * ones(11), logspace(-7,3,11),'k')
 #        p.xlim(0,0.01)
         p.title('z = '+str((z+0.5)*dz)+'m')
         p.xlabel('k (in 1/m')
         p.ylabel('Amplitude')
         p.savefig('z'+str(z)+'_Spectra.png')
+    
         

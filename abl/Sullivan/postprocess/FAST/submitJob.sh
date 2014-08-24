e5f32fb61f736fc1473e181b6ccb8f6a01c4ddeb
Ganesh Vijayakumar <ganesh10@login1.stampede.tacc.utexas.edu>
2014-08-24 15:42:40 -0500

    Changing compilation and submit options for Stampede

--- abl/Sullivan/postprocess/FAST/submitJob.sh
+++ abl/Sullivan/postprocess/FAST/submitJob.sh
@@ -1,15 +1,16 @@
 for (( i=1; i<=15; i++ ))
 do 
-    cp fast.pbs temp.pbs 
+    cp fast.slurm temp.slurm 
     echo $i 
-    sed "s/blah/$i/g" temp.pbs > temp1.pbs 
-    mv temp1.pbs fast_${i}_${i}.pbs 
+    sed "s/blah/$i/g" temp.slurm > temp1.slurm 
+    mv temp1.slurm fast_${i}_${i}.slurm 
 done
-cd /lustre/scratch/gantech/les_abl/Sullivan/data/sullivanTest/case037/FAST/
+cp dt /scratch/02504/ganesh10/ablFromKrakenAnalysis/FAST/
+cd /scratch/02504/ganesh10/ablFromKrakenAnalysis/FAST/
 cp ~/pseudoSpectralLES/abl/Sullivan/postprocess/FAST/FASTfilesWrite .
-cp ~/pseudoSpectralLES/abl/Sullivan/postprocess/FAST/fast_*.pbs .
-for (( i=2; i<=15; i++ ))
+cp ~/pseudoSpectralLES/abl/Sullivan/postprocess/FAST/fast_*.slurm .
+for (( i=1; i<=15; i++ ))
 do 
-    qsub fast_${i}_${i}.pbs
+    sbatch fast_${i}_${i}.slurm
 done
 cd -
